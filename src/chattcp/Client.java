85c3a231d7374d9c1cf3d643580099bc9c20859a
Tranhoainam197 <namth0943@ut.edu.vn>
2026-01-19 22:34:42 +0700

    client: handle client message sending

--- src/chattcp/Client.java
+++ src/chattcp/Client.java
@@ -1,2 +1,34 @@
+package chattcp;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client {
+
+    private final String SERVER_IP = "localhost";
+    private final int SERVER_PORT = 1436;
+
+    private Socket socket;
+    private BufferedReader reader;
+    private PrintWriter writer;
+    private volatile boolean isClosing = false;
+
+    public Client() {
+        try {
+            socket = new Socket(SERVER_IP, SERVER_PORT);
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            writer = new PrintWriter(socket.getOutputStream(), true);
+
+            System.out.println("Đã kết nối đến server: " + SERVER_IP + ":" + SERVER_PORT);
+
+        } catch (Exception e) {
+            System.err.println("Lỗi kết nối: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        new Client();
+    }
 }
